03b7cb46481f5b95a761613eba55d8ee6bc5dd4e
unknown <abdulmandaviya@gmail.com>
2026-03-07 14:36:48 +0530

    state, effect and ref

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import StateEffectRef from './components/StateEffectRef'
+
+function App() {
+
+  return (
+    <>
+      <StateEffectRef/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/StateEffectRef.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useRef, useState } from "react"
+
+const StateEffectRef = () => {
+    const [count, setCount] = useState(0)
+    const [timer, setTimer] = useState(0)
+    const [user, setUser] = useState([])
+
+    const nameRef = useRef()
+
+    const handleSubmit = () => { 
+        alert(nameRef.current.value)
+    }
+
+    useEffect(() => {
+       const interval = setInterval(() => { 
+            setTimer(prev => {
+                if(prev === 10) { 
+                    alert('Timer Up !!!')
+                    return 0
+                }
+                return prev + 1
+            })
+       },1000)
+
+       return () => clearInterval(interval)
+    },[])
+
+    useEffect(() => { 
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(data => setUser(data))
+    },[])
+
+    const decrementCount = () => {
+        if (count === 0) {
+            return
+        }
+        setCount(count - 1)
+    }
+
+    return (
+        <>
+            <h2> Use State, Use Effect and Use Ref</h2>
+            <div style={{ background: "pink", padding: "10px" }}>
+                <h4>timer</h4>
+                <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    <button style={{ cursor: "pointer" }} onClick={() => setCount(count + 1)}>Increment</button>
+                    Count is: {count}
+                    <button style={{ cursor: "pointer" }} onClick={() => decrementCount()}>Decrement</button>
+                </div>
+            </div>
+
+            <div style={{ background: "pink", padding: "10px", marginTop: "20px" }}>
+                <h4>Timer</h4>
+                <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    Timer Running: {timer}
+                    <button style={{ cursor: "pointer" }} onClick={() => setTimer(0)}>Reset</button>
+                </div>
+            </div>
+
+            <div style={{ background: "pink", padding: "10px", marginTop: "20px" }}>
+                <h4>Data from Api through use Effect</h4>
+                <ul>
+                   {user.map(item => (
+                    <li key={item.id}>{item.name}</li>
+                   ))}
+                </ul>
+            </div>
+
+            <div style={{ background: "pink", padding: "10px", marginTop: "20px" }}>
+                <h4>Form submit by use ref</h4>
+                <input type="text" ref={nameRef}/>
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+
+        </>
+    )
+}
+
+export default StateEffectRef
+
+// UseState:
+    // Used to store and update component state.
+    // Ex: Counters, Form Inputs, Toggle UI, Modal open/close, Loading state
+
+// UseEffect:
+    // Used for side effects in React.
+    // Api calls, DOM Updates, timers, event listners
+
+// Use Ref:
+    // stores mutable values without re-rendering
+    // commonly used in forms to directly access the DOM element (input field) without using state.
+    // This is useful for things like focusing an input, reading values, or clearing inputs.
+    // Difference between use state and use ref
+        // *** controlled components ****.
+        // -> when react needs contol on input then use use state
+        // -> Ex: Form Validation, live search, button enable/disable, real time UI updates.
+        // react always knows the latest values 
+        // user type -> onChange -> setstate -> component -> re-renders.
+
+        // *** uncontrolled components
+        // -> when we need value just once. usually on submit.
+        // -> Ex. simple forms, access DOM elements, focus inputs.
